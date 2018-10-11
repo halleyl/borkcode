ea9ceb16eb4ba71a93bb073061588034c58011f4
halleyl <halleyl@uw.edu>
2018-10-11 13:27:49 -0700

    Add form validation to index, edit some text

--- js/found.js
+++ js/found.js
@@ -4,6 +4,11 @@ var formButton = document.getElementById('submit')
 formButton.addEventListener("click", function() {
   event.preventDefault()
   var dogId = foundForm.dogId.value
+  if( dogId <= 0 || dogId > 2 ) {
+    var small = document.getElementById('message')
+    small.innerHTML = 'Dog ID not recognized. Please enter a valid number.'
+  } else {
   window.location.href = './found.html?dog=' + dogId
+  }
 })
 
